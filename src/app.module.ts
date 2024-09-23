22b89298c80db41ce822902852b7721c60eeb358
Abel Nutsugah <bigmanabel@outlook.com>
2024-09-23 12:15:48 +0000

    added hashing service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { IamModule } from './iam/iam.module';
 
 @Module({
   imports: [
@@ -18,7 +19,8 @@ import { ConfigModule } from '@nestjs/config';
       autoLoadEntities: true,
       synchronize: true,
     }),
-    UsersModule
+    UsersModule,
+    IamModule
   ],
   controllers: [AppController],
   providers: [AppService],
