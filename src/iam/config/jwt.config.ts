4ffcd6f6c7d8e3f6d85093dbd80682cd628bbb19
Abel Nutsugah <bigmanabel@outlook.com>
2024-09-23 14:10:14 +0000

    added refresh tokens

--- src/iam/config/jwt.config.ts
+++ src/iam/config/jwt.config.ts
@@ -6,5 +6,6 @@ export default registerAs('jwt', () => {
         audience: process.env.JWT_TOKEN_AUDIENCE,
         issuer: process.env.JWT_TOKEN_ISSUER,
         accessTokenTtl: +process.env.JWT_ACCESS_TOKEN_TTL || 3600,
+        refreshTokenTtl: +process.env.JWT_REFRESH_TOKEN_TTL || 86400,
     };
 });
