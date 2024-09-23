c6068cec69c1ce704c90b14a63c11114df429a89
Abel Nutsugah <bigmanabel@outlook.com>
2024-09-23 12:59:15 +0000

    jwt config

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,16 +1,22 @@
-import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { HashingService } from '../hashing/hashing.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from '../config/jwt.config';
+import { ConfigType } from '@nestjs/config';
+import { IsEmail } from 'class-validator';
 
 @Injectable()
 export class AuthenticationService {
     constructor(
         @InjectRepository(User) private readonly userRepository: Repository<User>,
         private readonly hashingService: HashingService,
+        private readonly jwtService: JwtService,
+        @Inject(jwtConfig.KEY) private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
     ) { }
 
     async signUp(signUpDto: SignUpDto) {
@@ -48,7 +54,21 @@ export class AuthenticationService {
             throw new BadRequestException('Password does not match');
         }
 
-        // TODO: Generate JWT token
-        return true;
+        const accessToken = await this.jwtService.signAsync(
+            {
+                sub: user.id,
+                email: user.email,
+            },
+            {
+                audience: this.jwtConfiguration.audience,
+                issuer: this.jwtConfiguration.issuer,
+                secret: this.jwtConfiguration.secret,
+                expiresIn: this.jwtConfiguration.accessTokenTtl,
+            },
+        );
+
+        return {
+            accessToken,
+        };
     }
 }
